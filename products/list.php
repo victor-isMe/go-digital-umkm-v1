4df2754a5728393c40ca2b12422f73da5d05b048
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-03-05 00:48:59 +0700

    Menambahkan file untuk menampilkan prouduk kepada customer

---
+++ products/list.php
@@ -0,0 +1,27 @@
+<?php
+require_once "../config/database.php";
+
+$stmt = $pdo->prepare("SELECT products.*, users.name AS umkm_name 
+                        FROM products 
+                        JOIN users ON products.user_id = user_id 
+                        ORDER BY products.created_at DESC");
+
+$product = $stmt->fetchAll();
+?>
+
+<h2>Daftar Produk UMKM</h2>
+<a href="../auth/login.php">Login</a>
+
+<hr>
+
+<?php foreach ($products as $product): ?>
+    <div>
+        <strong><?= htmlspecialchars($product["name"]) ?></strong><br>
+        UMKM: <?= htmlspecialchars($product['umkm_name']) ?><br>
+        Harga: Rp<?= $product["price"] ?><br>
+        Stok: <?= $product["stock"] ?><br>
+
+        <a href="detail.php?id=<?= $product["id"] ?>">Detail Produk</a>
+        <hr>
+    </div>
+<?php endforeach; ?>
