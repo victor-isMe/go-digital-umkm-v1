731db1765dabceef024144467f7bf7c2a3f3e840
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-03-08 00:02:08 +0700

    Mengubah file list.php agar bisa menampikan kategori produk

--- products/list.php
+++ products/list.php
@@ -1,9 +1,10 @@
 <?php
 require_once "../config/database.php";
 
-$stmt = $pdo->prepare("SELECT products.*, users.name AS umkm_name 
+$stmt = $pdo->prepare("SELECT products.*, users.name AS umkm_name, categories.name AS category_name 
                         FROM products 
-                        JOIN users ON products.user_id = user_id 
+                        JOIN users ON products.user_id = users.id 
+                        LEFT JOIN categories ON products.category_id = categories.id
                         ORDER BY products.created_at DESC");
 
 $product = $stmt->fetchAll();
@@ -17,6 +18,7 @@ $product = $stmt->fetchAll();
 <?php foreach ($products as $product): ?>
     <div>
         <strong><?= htmlspecialchars($product["name"]) ?></strong><br>
+        Kategori: <?= $product["category_name"] ?><br>
         UMKM: <?= htmlspecialchars($product['umkm_name']) ?><br>
         Harga: Rp<?= $product["price"] ?><br>
         Stok: <?= $product["stock"] ?><br>
