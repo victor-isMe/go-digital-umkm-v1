ad8cb10a4a20da85db0e809674a25624f08e299a
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-03-08 11:55:56 +0700

    Mengubah file list.php untuk bisa memfilter tampilan produk berdasarkan kategori

--- products/list.php
+++ products/list.php
@@ -1,19 +1,46 @@
 <?php
 require_once "../config/database.php";
 
-$stmt = $pdo->prepare("SELECT products.*, users.name AS umkm_name, categories.name AS category_name 
-                        FROM products 
-                        JOIN users ON products.user_id = users.id 
-                        LEFT JOIN categories ON products.category_id = categories.id
-                        ORDER BY products.created_at DESC");
+if (!empty($_GET["category"])) {
+    $category = $_GET["category"];
 
-$product = $stmt->fetchAll();
+    $stmt = $pdo->prepare("SELECT products.*, users.name AS umkm_name, categories.name AS category_name 
+                            FROM products 
+                            JOIN users ON products.user_id = users.id 
+                            LEFT JOIN categories ON products.category_id = categories.id
+                            WHERE products.category_id = ?
+                            ORDER BY products.created_at DESC");
+    $stmt->execute([$category]);
+} else {
+    $stmt = $pdo->query("SELECT products.*, users.name AS umkm_name, categories.name AS category_name
+                        FROM products
+                        JOIN users ON products.user_id=users.id
+                        LEFT JOIN categories ON products.category_id=categories.id
+                        ORDER BY products.created_at DESC");
+}
+
+$products = $stmt->fetchAll();
+$categories = $pdo->query("SELECT * FROM categories")->fetchAll();
 ?>
 
 <h2>Daftar Produk UMKM</h2>
 <a href="../auth/login.php">Login</a>
 
 <hr>
+
+<form method="GET">
+    <select name="category">
+        <option value="">Semua Kategori</option>
+
+        <?php foreach ($categories as $categori): ?>
+            <option value="<?= $categori["id"] ?>">
+                <?= $categori["name"] ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+
+    <button type="submit">Filter</button>
+</form>
 
 <?php foreach ($products as $product): ?>
     <div>
