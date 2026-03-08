1788c49bd68ddce83bf6dd2e02f3c456e202554d
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-03-08 12:11:23 +0700

    Memperbaiki bug pada koneksi database

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
-$dsn = 'mysql:host=$host;dbname=$db;charset=$charset';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
     PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
