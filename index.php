541ecc4d4695d38e05efe0e6f814810607f9e05f
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-04-01 08:59:21 +0700

    Mulai menyusun halaman utama

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "partials/header.php"; ?>
+
+<main style="min-height: 100vh;">
+    <h1>Test</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quam velit provident aliquam odio, dicta doloribus ad in fugit, labore hic quisquam, mollitia possimus quibusdam unde totam veritatis quas id?</p>
+</main>
+
+<?php include "partials/footer.php"; ?>
