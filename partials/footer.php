0482e77b91f7f376ca8cc9435eb05720e4c85c2e
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-04-03 19:47:20 +0700

    Mengisi konten untuk footer

---
+++ partials/footer.php
@@ -0,0 +1,31 @@
+<footer>
+    <div class="footer-info">
+        <div class="one">
+            <h3>Go Digital UMKM</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Impedit maxime nemo vero ea incidunt quos in sunt dolorum 
+                voluptates ex provident voluptatibus mollitia id expedita non, facere voluptatum sequi repudiandae.</p>
+        </div>
+    </div>
+
+    <div class="two">
+        <h3>Informasi Kontak</h3>
+        <ul>
+            <li>Surakarta, Jawa Tengah, Indonesia</li>
+            <li>(+62) xxx xxx xxx</li>
+            <li>xxx@gmail.com</li>
+        </ul>
+    </div>
+
+    <div class="three">
+        <h3>Ikuti Kami</h3>
+        <ul>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Youtube</a></li>
+        </ul>
+    </div>
+
+    <div class="footer">
+        <p>&copy; <?php echo date('Y'); ?> Go Digital UMKM</p>
+    </div>
+</footer>
